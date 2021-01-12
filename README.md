c3da243555c3e890fa8d194c535dbef23c753333
Barep Bimo Pangestu <bimo.pangestu09@gmail.com>
2021-01-13 00:08:08 +0700

    finished leak detection

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Tugas-Akhir  
-Sensor placement using genetic algorithm  
-use make-emitter [input name][output name] for inp files without emitter value  
-use try.py [input] to generate simulations  
-use GA-find-leak.py [input] [population size] to get sensor placement  
+leak detection using genetic algorithm  
+run find-leak.py [inp file] [csv file] [population size] [generations]  
+result will be saved in results.txt after program finished running
--- find-leak.py
+++ find-leak.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 import random
 import os
-import time
+from datetime import datetime
 
 inp_file = sys.argv[1]
 csv_file = sys.argv[2]
@@ -10,7 +10,11 @@ pop_size = int(sys.argv[3])
 generations = int(sys.argv[4])
 result = 'results.txt'
 
+lines = []
 results = []
+leaks = []
+final_results = [] #debug only
+save_fitness = [] #debug only
 simulation_dir = './temp'
 correct = 0
 incorrect = 0
@@ -52,7 +56,7 @@ def get_junctions():
 
 def create_species():
     temp = []
-    for i in range(len(junctions)):
+    for i in range(9):
         gen = round(random.uniform(0,1),2)
         temp.append(gen)
     return temp
@@ -72,32 +76,49 @@ def get_cases():
         print("CSV open error")
     return lines
 
+def generate_temp():
+    fileobj = open(inp_file,'r')
+    lines = fileobj.readlines()
+    fileobj.close()
+    fileout = open('temp.inp','w')
+    for i in range(len(lines)):
+        # print(lines[i])
+        fileout.write(lines[i])
+    fileout.close()
+
 def change(category, idname, pos,val):
-    try:
-        fileobj = open(inp_file,'r')
-        lines = fileobj.readlines()
-        fileobj.close()
-    except:
-        print("inp file open error")
-    try:
-        fileout = open("temp.inp",'w')
-    except:
-        print("temp file open error")
+    # print(idname)
+    fileout = open('temp.inp','r+')
+    lines = fileout.readlines()
+    fileout.seek(0)
+    fileout.truncate()
 
     incategory = False
     nl = len(lines)
     for i in range(nl):
-        ss=str(lines[i].split())
+        # print(lines[i])
+        ss=lines[i].split()
+        # print(type(ss))
         if (ss):
+            # print(ss[0])
             if (incategory):
+                
                 if (len(ss) <= 1):
                     incategory = False
                 else:
-                    if ((ss[0].find(idname) > -1) or (idname=='*' and ss[0][0]!=';')):
-                        ss[pos] = val
+                    # print(ss[0])
+                    if ((ss[0].find(str(idname)) > -1) or (str(idname)=='*' and ss[0][0]<>';')):
+                        # print('ss')
+                        ss[pos] = str(val)
+                        ss.append('\n')
+                        # print(ss)
                         lines[i] = '\t'.join(ss)
+                        # print(lines[i])
+                        # print(i)
             if (ss[0].find(category) > -1):
                 incategory = True
+        # print(lines[i])
+        # print(i)
         fileout.write(lines[i])
     fileout.close()
 
@@ -117,11 +138,13 @@ def getflows(linkfile):
     return flows
 
 def find_fitness(species, flows):
+    generate_temp()
     fitness = 0.0
     for i in range(len(species)):
-        change('EMITTERS', i, 1, species[i])
+        change('EMITTERS', str(junctions[i]), 1, species[i])
     subprocess.call(['java', '-cp', 'AwareEpanetNoDeps.jar', 'org.addition.epanet.EPATool', 'temp.inp'])
     species_flow = getflows('temp.inp.links.out')
+    # print(species_flow)
     for i in range(len(flows)):
         fitness = fitness + abs(float(flows[i]) - float(species_flow[i]))
     return round(fitness, 2)
@@ -134,80 +157,96 @@ def mutate(chromosome):
     return chromosome
 
 def crossover(parent1, parent2):
+    cross_point = random.randint(0,len(parent1) - 1)
     # print("parent 1: " + str(parent1))
     # print("parent 2: " + str(parent2))
-    child = parent1[:pipe_count/2] + parent2[pipe_count/2:]
+    child = parent1[:cross_point] + parent2[cross_point:]
     # print("child: " + str(child))
     mutate(child)
     return child
 
-def GA(pop, line):
+def GA(pop, flows):
+    global save_fitness
     # print("Population: ")
     # for i in range(len(pop)):
     #     print(pop[i])
-    flows = []
     fitness = []
     new_pop = []
-    for i in range(pipe_count):
-        flows.append(round(float(line[i+2]),2))
     for i in range(len(pop)):
         # print(i)
         fitness.append(find_fitness(pop[i], flows))
+    # print(fitness)
+    # save_fitness.append(fitness)
     sorted_pop = [pop for _, pop in sorted(zip(fitness,pop))]
     # print("Sorted population: ")
     # for i in range(len(sorted_pop)):
     #     print(sorted_pop[i])
-    for i in range(int(pop_size * 0.75)):  
+    for i in range(int(pop_size * 0.5)):  
         new_pop.append(sorted_pop[i])
     for i in range(int(pop_size * 0.25)):
-        new_pop.append(crossover(new_pop[i],new_pop[i + int(pop_size / 4)]))
+        new_pop.append(crossover(new_pop[i],sorted_pop[random.randint(0,len(pop)-1)]))
+    for i in range(int(pop_size * 0.25)):
+        new_pop.append(create_species())
 
     return new_pop
 
 def check_result(species, answer):
+    global leaks
     global correct
     global incorrect
+    species_copy = species[:]
     leak = []
     for i in range(3):
-        biggest_leak = species.index(max(species))
+        biggest_leak = species_copy.index(max(species_copy))
         leak.append(junctions[biggest_leak])
-        species[biggest_leak] = 0.0
+        species_copy[biggest_leak] = 0.0
+    leaks.append(leak)
     if (answer in leak):
         correct += 1
     else:
         incorrect +=1
 
 def detect_leak(case):
+    global final_results
     flows = []
     fitness = []
     case = case.split(',')
     population = initial_pop(pop_size)
     # for i in range(len(population)):
     #     print (population[i])
-    for i in range(generations):
-        population = GA(population, case)
-        # print(len(population))
     for i in range(pipe_count):
         flows.append(round(float(case[i+2]),2))
+    for i in range(generations):
+        population = GA(population, flows)
+        # print(len(population))
     for i in range(len(population)):
-        fitness.append(find_fitness)(population[i], flows)
+        fitness.append(find_fitness(population[i], flows))
     sorted_pop = [population for _, population in sorted(zip(fitness, population))]
+    # print(len(sorted_pop))
+    final_results.append(sorted_pop[0])
     check_result(sorted_pop[0], case[1])
     return sorted_pop[0]
 
+start = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 get_junctions()
 cases = get_cases()
-for i in range(len(cases)/4):
-    results.append(detect_leak(cases[random.randint(9,len(cases))]))
+for i in range(100):
+    case = random.randint(9,len(cases))
+    lines.append(case)
+    results.append(detect_leak(cases[case-1]))
+end = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 fileout = open(result, 'w')
-fileout.write("Test case = " + str(correct+incorrect))
-fileout.write("Correct prediction = " + str(correct))
-fileout.write("Incorrect prediction = " + str(incorrect))
+fileout.write("Start time: " + start + " End time: " + end + '\n')
+fileout.write("Test case = " + str(correct+incorrect) + "\n")
+fileout.write("Correct prediction = " + str(correct) + "\n")
+fileout.write("Incorrect prediction = " + str(incorrect) + '\n')
+# for i in range(len(save_fitness)):
+#     fileout.write(str(save_fitness[i]) + '\n')
 for i in range(len(results)):
-    fileout.write(str(results[i]))
+    fileout.write(str(lines[i]) + ' ' + str(leaks[i]) + ' ' + str(results[i]) + '\n')
 # for i in range(len(cases)):
 #     fileout.write(detect_leak(cases[i]))
 fileout.close()
-os.remove("temp.inp")
-os.remove("temp.inp.links.out")
-os.remove("temp.inp.nodes.out")
+# os.remove("temp.inp")
+# os.remove("temp.inp.links.out")
+# os.remove("temp.inp.nodes.out")
